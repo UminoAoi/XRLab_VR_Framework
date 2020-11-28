f54f7e4a08b344ddd0d2c440d6c80da5df019de5
Vlad-at-PJATK <s23401@pjwstk.edu.pl>
2020-11-28 22:07:18 +0100

    Game 0.1

---
+++ Assets/Scripts/Character/Foe.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Foe : MonoBehaviour
+{
+ 
+     public Transform Player;
+     int MoveSpeed = 4;
+     int MinDist = 1;
+     public int is_selected = 0;
+ 
+ 
+ 
+ 
+     void Start()
+     {
+        Scene scene = gameObject.scene;
+        Debug.Log(gameObject.name + " is from the Scene: " + scene.name);
+        Debug.Log(scene.GetRootGameObjects()[1].name);
+        // Player = GameObject.FindWithTag("Player").transform;
+     }
+ 
+     void Update()
+     {
+         transform.LookAt(Player);
+ 
+         if ((Vector3.Distance(transform.position, Player.position) >= MinDist) && (is_selected != 0))
+         {
+ 
+             transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+ 
+ 
+ 
+             if (Vector3.Distance(transform.position, Player.position) <= MinDist)
+             {
+                 //EndGame
+             }
+ 
+         }
+     }
+ }
---
+++ Assets/Scripts/Room/RoomLogic.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomLogic : MonoBehaviour
+{
+    float ranPos;
+    public GameObject[] foes;
+    public GameObject selected;
+    public float targetTime;
+    Foe referenceScript;
+
+    public Transform Foe_prefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Instantiate(Foe_prefab, new Vector3(Random.Range(-20.0f, 20.0f), 1, Random.Range(-20.0f, 20.0f)), Quaternion.identity);
+            // Debug.Log();
+        }
+
+        foes = GameObject.FindGameObjectsWithTag("Foe");
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetTime -= Time.deltaTime;
+
+        if(targetTime < 0.1f)
+        {
+            killall();
+            updateTime();
+        }
+
+        if((targetTime < 1.0f) && (referenceScript.is_selected == 0))
+        {
+            undead();
+            Debug.Log("ASS");
+        }
+
+
+
+    }
+
+
+    void updateTime()
+    {
+        targetTime = 5.0f;
+    }
+
+
+    void undead()
+    {
+        selected = foes[Random.Range(0, 11)];
+        referenceScript = selected.GetComponent<Foe>();
+        referenceScript.is_selected = 1;
+    }
+
+    void killall()
+    {
+        foreach (GameObject Foee in foes)
+        {
+            referenceScript = Foee.GetComponent<Foe>();
+            referenceScript.is_selected = 0;
+        }
+    }
+}
