1d4d8e95b7e82e198cc99c38983a36683e14e19d
Vlad-at-PJATK <s23401@pjwstk.edu.pl>
2020-11-29 01:30:27 +0100

    Fixed

---
+++ Assets/Scripts/Character/Interact.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Interact : Interactable
+{
+
+    private void Awake() {
+    }
+
+
+    public override void OnInteraction(HandManager handManager, PointerEventArgs args) {
+        Destroy(this);
+    }
+
+    public override void OnPointerEnter(HandManager handManager, PointerEventArgs args) {
+    }
+
+    public override void OnPointerExit(HandManager handManager, PointerEventArgs args) {
+    }
+
+    internal void HideText() {
+        // text.gameObject.SetActive(false);
+    }
+}
